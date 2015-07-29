6506cd89a7d61a6a8b34db73c31f77c6db6c08a5
Peter Marshall <p.s.marshall0@gmail.com>
2015-07-29 15:45:00 +1000

    Make the world less refcount-centric and hopefully a bit faster

--- hphp/runtime/base/apc-string.cpp
+++ hphp/runtime/base/apc-string.cpp
@@ -28,7 +28,7 @@ APCString::MakeSharedString(DataType type, StringData* data) {
   auto apcStr       = new (mem) APCString(type);
 
   apcStr->m_str.m_data        = reinterpret_cast<char*>(apcStr + 1);
-  apcStr->m_str.m_hdr.init(cc, HeaderKind::String, 0);
+  apcStr->m_str.m_hdr.init(cc, HeaderKind::String, UnsharedGCByte);
   apcStr->m_str.m_len         = len; // don't store hash
 
   apcStr->m_str.m_data[len] = 0;
